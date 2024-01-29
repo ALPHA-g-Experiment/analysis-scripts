1f77d2b4104d4b17d1f4ddb9460aaae91d9788d3
Daniel Duque <daniel.duque@cern.ch>
2024-01-28 22:43:26 -0800

    Map scaler times to uniform distribution within readout events

--- bin/trg_scalers.py
+++ bin/trg_scalers.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 import polars as pl
 
 parser = argparse.ArgumentParser(
@@ -18,6 +19,39 @@ parser.add_argument("--remove-input-counter", action="store_true")
 parser.add_argument("--remove-output-counter", action="store_true")
 args = parser.parse_args()
 
+columns = {
+    "input": not args.remove_input_counter,
+    "drift_veto": args.include_drift_veto_counter,
+    "scaledown": args.include_scaledown_counter,
+    "pulser": args.include_pulser_counter,
+    "output": not args.remove_output_counter,
+}
+
 df = pl.read_csv(args.infile, comment_prefix="#").filter(
     pl.col("trg_time").is_between(args.t_min, args.t_max)
 )
+# All histograms should have the same binning
+_, t_edges = np.histogram(df["trg_time"], bins=args.t_bins)
+
+for name, included in columns.items():
+    if not included:
+        continue
+    """
+    We only know the time of the output counters. For all the other ones we just
+    know by how much they were incremented. The best we can do is assume that
+    those counts are evenly spread out over the time interval.
+    """
+    col_df = (
+        df.filter(pl.col(name).is_not_null())
+        .rename({"trg_time": "t_right"})
+        .with_columns(
+            (pl.col("t_right") - pl.col("t_right").diff()).alias("t_left"),
+            pl.col(name).diff().alias("counts"),
+        )
+        .filter(pl.col("counts") > 0)
+        .select(["t_left", "t_right", "counts"])
+    )
+
+    times = []
+    for t_left, t_right, counts in col_df.rows():
+        times.extend(np.linspace(t_left, t_right, counts + 1)[1:])
